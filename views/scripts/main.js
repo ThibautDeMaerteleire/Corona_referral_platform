60228e2663ebb0802d838993ae223ba638ca227f
Codelicious <thibdema@student.arteveldehs.be>
2020-10-15 22:03:12 +0200

    auto commit

--- views/scripts/main.js
+++ views/scripts/main.js
@@ -1,3 +1,5 @@
 import '../../node_modules/popper.js/dist/popper.js';
 import '../../node_modules/jquery/dist/jquery.js';
 import '../../node_modules/bootstrap/dist/js/bootstrap';
+import '';
+import "js/bootstrap-dropdownhover.min.js";
